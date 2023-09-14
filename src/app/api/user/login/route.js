8b929e53b824cf3cb895415f25254a5cbd95c912
Phurinon <folk.5834@gmail.com>
2023-09-14 15:15:30 +0700

    finish

--- src/app/api/user/login/route.js
+++ src/app/api/user/login/route.js
@@ -2,19 +2,31 @@ import jwt from "jsonwebtoken";
 
 import { DB, readDB } from "@/app/libs/DB";
 import { NextResponse } from "next/server";
+import { headers } from "next/headers";
 
 export const POST = async (request) => {
   readDB();
+  const body = await request.json();
+  const { username, password } = body;
+  const user = DB.users.find(
+    (user) => user.username === username && user.password === password
+  );
+  if (!user) {
+    return NextResponse.json(
+      {
+        ok: false,
+        message: "Username or Password is incorrect",
+      },
+      { status: 400 }
+    );
+  }
 
-  // return NextResponse.json(
-  //   {
-  //     ok: false,
-  //     message: "Username or Password is incorrect",
-  //   },
-  //   { status: 400 }
-  // );
+  const payload = {
+    username,
+    role: user.role,
+  };
 
-  const token = "Replace this with token creation";
+  const token = jwt.sign(payload, process.env.JWT_SECRET);
 
   return NextResponse.json({ ok: true, token });
 };
